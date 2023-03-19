4a951d4d8b82ea628f71449c5a513cec70549e75
khalids01 <khalids8774@gmail.com>
2023-03-19 20:38:17 +0600

    tailwind and redux setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <nav>
+          <h4 className="text-teal-500 font-bold text-6xl">Header</h4>
+        </nav>
+      </div>
+    </main>
+  );
+}
+
+export default App;
