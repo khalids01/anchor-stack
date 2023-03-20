addf992e97f19b49bafe481ef94e6525342c68b7
khalids01 <khalids8774@gmail.com>
2023-03-20 22:30:33 +0600

    nav 1

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,41 @@
+import images from "../constants/images";
 const Nav = () => {
+  const languages = [
+    {
+      value: "english-uk",
+      label: "English (UK)",
+    },
+    {
+      value: "english-us",
+      label: "English (US)",
+    },
+  ];
+
   return (
     <header>
-      <nav className=" container mx-auto flex bg-slate-200 justify-between items-center px-6 py-2 ">
+      <nav className="py-7 container mx-auto flex bg-white justify-between items-center px-6 py-2 ">
         <img
           className="object-contain max-w-40 h-full max-h-11"
           src="../assets/logo.png"
         />
-        <select className="bg-light px-5 px-2 ">
-          <option value={"english(uk)"}>English (UK)</option>
-          <option value={"english(us)"}>English (US)</option>
-        </select>
+
+        <div className="w-max relative ">
+          <select
+            className=" appearance-none bg-light cursor-pointer outline-none pl-4 pr-14 py-3 text-muted  rounded-xxl"
+            style={{ fontWeight: 500, fontSize: 12 }}
+          >
+            {languages.map((ln, index) => (
+              <option value={ln.value} key={index}>
+                {ln.label}
+              </option>
+            ))}
+          </select>
+          <img
+            src={`../assets/${images.chevron}`}
+            className="w-2.5 absolute right-3 top-4 translate-y-0.5"
+            alt="down"
+          />
+        </div>
       </nav>
     </header>
   );
---
+++ constants/images.ts
@@ -0,0 +1,21 @@
+const images = {
+    logo: 'logo.png',
+    logo_icon: 'logo-icon.png',
+    apple: 'apple.png',
+    at: 'at.png',
+    chevron: 'chevron.png',
+    user: 'user.png',
+    dashboard: 'dashboard.png',
+    google: 'google.png',
+    name: 'name.png',
+    sales: 'sales.png',
+    lock: 'lock.png',
+    last: 'last.png',
+    search: 'search.png',
+    bell: 'bell.png',
+    dots: 'dots.png',
+    eye: 'eye.png',
+    first: 'first.png'
+}
+
+export default images
